13bb32ea336b04f1b72fb69f590bb6f950006c07
Sabu Siyad <hello@ssiyad.com>
2024-03-08 11:01:45 +0530

    feat: get editor from env

--- edit/src/lib.rs
+++ edit/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -21,9 +22,13 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 fn editor(content: String) -> Result<String, Box<dyn Error>> {
     let mut temp_file = NamedTempFile::new()?;
     temp_file.write_all(content.as_bytes())?;
-    Command::new("nvim").arg(temp_file.path()).status()?;
+    Command::new(get_editor()).arg(temp_file.path()).status()?;
     let mut edited_file = File::open(temp_file.path())?;
     let mut edited_output = String::new();
     edited_file.read_to_string(&mut edited_output)?;
     Ok(edited_output)
+}
+
+fn get_editor() -> String {
+    env::var("EDITOR").unwrap_or("vim".to_string())
 }
