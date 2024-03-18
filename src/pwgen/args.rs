8686298c8ef57551829cf56f6a68318df452ecd7
Sabu Siyad <hello@ssiyad.com>
2024-03-18 19:08:37 +0530

    feat: `pwgen`: flags for symbols and digits

--- src/pwgen/args.rs
+++ src/pwgen/args.rs
@@ -2,6 +2,15 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
+    /// Desired length of the password
     #[arg(index = 1, default_value = "16")]
     pub length: usize,
+
+    /// Include digits
+    #[arg(short, long, default_value = "false")]
+    pub digits: bool,
+
+    /// Include special characters
+    #[arg(short, long, default_value = "false")]
+    pub symbols: bool,
 }
--- src/pwgen/mod.rs
+++ src/pwgen/mod.rs
@@ -5,12 +5,17 @@ pub use args::Args;
 pub use generator::Generator;
 
 pub fn main(args: Args) {
-    let mut generator = Generator::new()
-        .with_lowercase()
-        .with_uppercase()
-        .with_digits()
-        .with_special()
-        .prepare();
+    let mut generator = Generator::new().with_lowercase().with_uppercase();
+
+    if args.digits {
+        generator = generator.with_digits();
+    }
+
+    if args.symbols {
+        generator = generator.with_special();
+    }
+
+    generator = generator.prepare();
 
     for _ in 0..10 {
         println!("{}", generator.generate(args.length));
